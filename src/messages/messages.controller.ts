89c9f68ad989435ca524abc02ecc350e08d3c331
alanbrunoscience <alanbrunomath@gmail.com>
2025-11-06 22:39:51 -0300

    :sparkles: Update the 'MessagesController' class

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -2,13 +2,15 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
-  @Get()
+  @Get() // Maps HTTP requests of type GET (GET /messages)
   findAll() {
     return 'This action returns all messages';
   }
 
-  @Get(':id')
+  @Get(':id') // Adds the @Get decorator with the route parameter :id (GET /messages/:id)
+  // Adds the @Param decorator to capture the ID
   findOne(@Param('id') id: string) {
+    // The return value can be adjusted to include the ID for educational purposes
     return `This action returns the message #${id}`;
   }
 }
